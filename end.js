b11866bcc641d5622aba858b2471cf4e3169f729
Amir Behzad <a.behzad.ebr@gmail.com>
2020-01-18 22:39:01 +0300

    The end page keeps the the highscore

---
+++ end.js
@@ -0,0 +1,13 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
+username.addEventListener('keyup', () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = e => {
+  console.log("Clicked the save button");
+  e.preventDefault();
+};
