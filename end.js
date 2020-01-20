c926ab958488406fc357898c7ae80a8641324a0d
Amir Behzad <a.behzad.ebr@gmail.com>
2020-01-20 23:13:36 +0300

    The end.js is implemented and it does the job

--- end.js
+++ end.js
@@ -2,12 +2,31 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
+
 username.addEventListener('keyup', () => {
   saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = e => {
-  console.log("Clicked the save button");
+  console.log("Clicked the save button!");
   e.preventDefault();
+
+  const score = {
+    // score: mostRecentScore,
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  // A good syntax for sorting
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign("/");
 };
